471d93037292ba4d07aabbc678f1e448b181d09c
Rutik Maraskolhe <84556881+Cretus-iosDev@users.noreply.github.com>
2023-03-08 10:30:33 +0530

    Add files via upload

---
+++ PieChart/PieChart/PieChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieChartApp.swift
+//  PieChart
+//
+//  Created by Dr. Shrikant Maraskolhe on 08/03/23.
+//
+
+import SwiftUI
+
+@main
+struct PieChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
